8c695c28be3d9b752b76893ce0c7d521cc96806b
José Lisboa <104661333+Proceed15@users.noreply.github.com>
2024-05-05 22:11:13 -0300

    Update IA_Atv1_Casas.py

--- IA_Atv1_Casas.py
+++ IA_Atv1_Casas.py
@@ -7,15 +7,17 @@ def Casas():
     print("Bem-vindo(a) ao Descobridor de Valor!")
     print("Digite as informações da casa, uma por vez.")
     num_de_quartos = int(input("Digite o número de quartos: "))
-    largura = int(input("Digite a largura: "))
-    altura = int(input("Digite a altura: "))
-    dimensoes = largura*altura
-    vizinhanca = int(input("digite o número de casas ao redor: "))
+    largura = int(input("Digite a largura em metros: "))
+    altura = int(input("Digite a altura em metros: "))
+    dimensoes = (largura*altura)*10000
+    vizinhanca = int(input("Digite o número de casas ao redor: "))
     valor = Estimativa_do_preco_de_casas(num_de_quartos, dimensoes, vizinhanca)
+    retorna = "{0:.2f}".format(valor)
     
-    print("O preço da residência é: " + str(valor) + "$.")
+    print("O preço dessa casa é: " + retorna + "$.")
 
 def Estimativa_do_preco_de_casas(num_de_quartos, dimensoes, vizinhanca):
+# Essa é uma função para Inteligência Artificial
  preco = 0
 # Uma pitada disso
  preco += num_de_quartos * 0.123
