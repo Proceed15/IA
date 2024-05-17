c6c49efe49b23f7715c91e2842fe5da39c095250
José Lisboa <104661333+Proceed15@users.noreply.github.com>
2024-05-17 12:21:38 -0300

    Update FaceProjector.py

--- IA_Atv3_FaceDecoder/FaceProjector.py
+++ IA_Atv3_FaceDecoder/FaceProjector.py
@@ -3,42 +3,44 @@ import dlib
 import cv2
 import openface
 
-# You can download the required pre-trained face detection model here:
+# Você pode baixar e instaar o modelo de detecção de faces (do dlib), já pré-treinado e necessário a essa atividade, aqui:
 # http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+# Irá automaticamente baixar o arquivo, deixei o zip caso prefira baixar desse repositório nesta pasta.
 predictor_model = "shape_predictor_68_face_landmarks.dat"
 
-# Take the image file name from the command line
+# Pega o nome do arquivo de imagem da linha de comando (CMD prompt).
 file_name = sys.argv[1]
 
-# Create a HOG face detector using the built-in dlib class
+# Cria o detector de faces HOG usando a já implatada classe do dlib (por isso e mais é importante instalá-la em seu computador antes).
 face_detector = dlib.get_frontal_face_detector()
 face_pose_predictor = dlib.shape_predictor(predictor_model)
 face_aligner = openface.AlignDlib(predictor_model)
 
-# Take the image file name from the command line
+# Pega o nome do arquivo de imagem da linha de comando (CMD prompt).
 file_name = sys.argv[1]
 
-# Load the image
+# Carrega a imagem.
 image = cv2.imread(file_name)
 
-# Run the HOG face detector on the image data
+# Usa o detector de faces HOG nos dados da imagem.
 detected_faces = face_detector(image, 1)
 
 print("Found {} faces in the image file {}".format(len(detected_faces), file_name))
 
-# Loop through each face we found in the image
+# Mostra a janela na área de trabalho com a imagem.
 for i, face_rect in enumerate(detected_faces):
 
-	# Detected faces are returned as an object with the coordinates 
-	# of the top, left, right and bottom edges
+	# As faces identificadas são retornadas como um objeto com... 
+	# coordenadas da parte de cima, esquerda, direita e partes inferiores.
 	print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
 
-	# Get the the face's pose
+	# Pega a posição em que a face está (de lado, centralizada).
 	pose_landmarks = face_pose_predictor(image, face_rect)
-
-	# Use openface to calculate and perform the face alignment
+	
+	# Nesta etapa precisamos ter utilizado o algoritmo do FaceFinder para encontrar os marcos do rosto na imagem.
+	# Usa openface para calcular os marcos do rosto e fazer um alinhamento centralizado desta face.
 	alignedFace = face_aligner.align(534, image, face_rect, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
 
-	# Save the aligned image to a file
+	# Salva a imagem alinhada em um arquivo .jpg.
 	cv2.imwrite("aligned_face_{}.jpg".format(i), alignedFace)
 	        
